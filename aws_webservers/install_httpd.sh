cd872f5ea4c76434dc932186adc109d9988fd7f6
EC2 Default User <ec2-user@ip-172-31-92-242.ec2.internal>
2025-10-26 21:51:26 +0000

    Update webserver user data: Change Week 4 to Week 5.

--- aws_webservers/install_httpd.sh
+++ aws_webservers/install_httpd.sh
@@ -2,6 +2,6 @@
 yum -y update
 yum -y install httpd
 myip=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
-echo "<h1>Welcome to ACS730 Week 4, Session 2! My private IP is $myip</h1><br>Built by Terraform!"  >  /var/www/html/index.html
+echo "<h1>Welcome to ACS730 Week 5, Session 2! My private IP is $myip</h1>"  >  /var/www/html/index.html
 sudo systemctl start httpd
 sudo systemctl enable httpd
